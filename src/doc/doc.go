3a0a9807a9ce321640f5093303914185d8ff0ff5
Elis Hirwing <elis@hirwing.se>
2022-05-05 16:52:26 +0200

    add image examples for all modules to the readme

--- src/doc/doc.go
+++ src/doc/doc.go
@@ -11,7 +11,7 @@ func WrapDocObjects(specFile spec.SpecFile, docObjects []string) string {
 	var objects []string
 
 	objects = append(objects, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>")
-	objects = append(objects, fmt.Sprintf("<svg width=\"%d\" height=\"%d\">", specFile.Width, specFile.Height))
+	objects = append(objects, fmt.Sprintf("<svg width=\"%d\" height=\"%d\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">", specFile.Width, specFile.Height))
 
 	for _, v := range docObjects {
 		objects = append(objects, v)
