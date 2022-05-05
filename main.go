3d8b7dd8bbafda05a1f257f544b5092b6da8b8a1
Elis Hirwing <elis@hirwing.se>
2022-05-05 09:52:56 +0200

    Put rendering of modules and other things together to output SVG

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 
+	"github.com/etu/absvgen/src/doc"
+	"github.com/etu/absvgen/src/modules"
 	"github.com/etu/absvgen/src/spec"
 )
 
@@ -18,6 +20,25 @@ func main() {
 	flag.StringVar(&specFile, "spec", "spec.yaml", "Specify path to the spec.yaml file")
 	flag.Parse()
 
+	// Parse the specification
 	spec := spec.New(specFile)
-	fmt.Println(spec)
+
+	// Slice to store render results in
+	docObjects := []string{}
+
+	for _, v := range spec.Layers {
+		// Get the module we want to use
+		module := modules.Get(v.Module)
+
+		// Render the module
+		moduleResult := module.Render(v, spec)
+
+		// Store the result in the slice
+		docObjects = append(docObjects, moduleResult)
+	}
+
+	// Wrap the SVG objects from the different modules in SVG header/footer
+	svg := doc.WrapDocObjects(spec, docObjects)
+
+	fmt.Println(svg)
 }
