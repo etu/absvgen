959c7ca2cfe18d3e5fa02630382e439210f0e766
Elis Hirwing <elis@hirwing.se>
2022-05-05 08:37:15 +0200

    parse command line flags

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main // import "github.com/etu/absvgen"
 
+import (
+	"flag"
+)
+
 func main() {
+	var specFile string
+	var debug bool
+
+	//
+	// Parse command line flags
+	//
+	flag.BoolVar(&debug, "debug", false, "Enable or disable debug output")
+	flag.StringVar(&specFile, "spec", "spec.yaml", "Specify path to the spec.yaml file")
+	flag.Parse()
 }
